ce9efc7bbd5a3fd190f2d5626a65cdc9599552d4
spencerleekramer <spencerkramer.sk@gmail.com>
2016-07-09 01:55:22 -0400

    Create corporation.ts

---
+++ src/scripts/corporation.ts
@@ -0,0 +1,11 @@
+import Resource = require('./resource');
+import Collection = require('./collection');
+
+class Corporation {
+  name: string;
+  corporateID: string;
+  moneyOut:number;
+  influenceOut: number;
+  }
+  
+  export = Corporation;
